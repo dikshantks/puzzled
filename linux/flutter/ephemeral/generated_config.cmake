7b08f5e887f917f360a072044847e4161499da6e
dikshantks <dikshantks229@gmail.com>
2022-01-18 14:54:39 +0530

    "first commit"

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/dikshant/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/dikshant/Desktop/flutter/chatapp_eg/puzzled" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/dikshant/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/dikshant/Desktop/flutter/chatapp_eg/puzzled"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/dikshant/Desktop/flutter/chatapp_eg/puzzled/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/dikshant/Desktop/flutter/chatapp_eg/puzzled/lib/main.dart"
+)
