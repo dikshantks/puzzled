977b90854522173c1799ecdb1242ca358226a71f
dikshantks <dikshantks229@gmail.com>
2022-01-19 23:27:22 +0530

    compiled

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: camel_case_types, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,23 +13,99 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      home: homepage(),
+      home: WelcomeScreen(),
     );
   }
 }
 
-class homepage extends StatelessWidget {
-  const homepage({
-    Key? key,
-  }) : super(key: key);
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(""),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 50.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 5.0,
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Tallete",
+                      style: GoogleFonts.notoSansMono(
+                        textStyle: TextStyle(
+                          fontSize: 50.0,
+                          color: Color(0xff3434ff),
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                  child: Text(
+                    "APP for managing your notes",
+                    style: GoogleFonts.notoSansMono(
+                      textStyle: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40.0),
+                  child: Container(
+                    height: 300.0,
+                    color: Colors.blueGrey,
+                    child: Image.asset(
+                      "assets/images/img1.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 60.0),
+              // ignore: deprecated_member_use
+              child: RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 50.0),
+                color: Color(0xff3434ff),
+                child: Text(
+                  "LOGIN",
+                  style: GoogleFonts.notoSansMono(
+                    textStyle: TextStyle(
+                      fontSize: 30.0,
+                      color: Color(0xffa34343),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
