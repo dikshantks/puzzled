3c6d5c8dd638afe6051d9275b6f3c6e8f84cf5b6
dikshantks <dikshantks229@gmail.com>
2022-01-28 16:59:26 +0530

    override a commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,26 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-<<<<<<< HEAD
       home: EnterScreen(),
-=======
-      home: homepage(),
-    );
-  }
-}
-
-class homepage extends StatelessWidget {
-  const homepage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("DK is a duche dick!!!!"),
-      ),
->>>>>>> 35a3b198df4e7dbfc22e7d59e4c406f27689ecda
     );
   }
 }
